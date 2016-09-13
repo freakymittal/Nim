adbdbbcb5aa7ed51e201ad481f6c48546503be30
Mayank Mittal <mayank_mittal@outlook.com>
2016-09-14 00:58:11 +0530

    Update Nim_main.cpp

--- Nim_main.cpp
+++ Nim_main.cpp
@@ -10,9 +10,11 @@ const int no_heap[3] = {3, 5, 7};
 const int heap_length[3] = {9, 11, 13};
 string players[2] = {"Computer", "Human"};
 int heaps,heap[3];
-int choose(string player)
+void choose(string player)
 {
 	int x,y;
+	string msg="Player Human enter the number of objects (Y) to take from what heap (X) - in order: Y X\nHuman Enters : ";
+	string err="\nPlayer Human that is an invalid move, try again\n";
 	if(player=="Computer")
 	{
 		x = rand() % heaps;
@@ -23,26 +25,24 @@ int choose(string player)
 	}
 	else if(player=="Human")
 	{
-		cout<<"Player Human enter the number of objects (Y) to take from what heap (X) - in order: Y X\nHuman Enters : ";
+		cout<<msg;
 		cin>>y>>x;
 		while(y>heap[x-1]){
-			cout<<"\nPlayer Human that is an invalid move, try again\nPlayer Human enter the objects (Y) to take from what heap (X) - in order: Y X\nHuman Enters : ";
+			cout<<err<<msg;
 			cin>>y>>x;		
 		}
 		x--;
 	}
 	cout<<"Player "<<player<<" took "<< y <<" objects from heap "<<x+1<<"\n";
 	heap[x]-=y;
-	return 0;
 }
-int print_heap()
+void print_heap()
 {
 	for(int i=0;i<heaps;i++)
 	{
 		cout<<heap[i]<<setw(4);
 	}
 	cout<<"\n";
-	return 0;
 }
 int check(const int a[], int n)
 {   
